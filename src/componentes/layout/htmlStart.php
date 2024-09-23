5531678066160f00cdcf23a3999601281a7149e9
Rubens Flinco <contato@rubensflinco.com.br>
2024-09-23 02:54:33 -0300

    tags

--- src/componentes/layout/htmlStart.php
+++ src/componentes/layout/htmlStart.php
@@ -26,6 +26,19 @@
     <link href="<?php echo $pathUrl;?>/vendor/enyo/dropzone/dist/min/dropzone.min.css" rel="stylesheet"/>
     <script type="text/javascript" src="<?php echo $pathUrl;?>/vendor/enyo/dropzone/dist/min/dropzone.min.js"></script>
 
+    <!-- Tags Snippets -->
+    <meta name="msvalidate.01" content="95945B5C333979BFC9AB593DAC527532" />
+    <meta name="google-adsense-account" content="ca-pub-7169391558658534">
+    <meta name="google-site-verification" content="59YXkT3_q1FWwkUC3eMV9BrTTAEOBfDcVXsVSU4A8k8" />
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "o7ki2sfjw7");
+    </script>
+
+
 </head>
 
 <div class="theme-body">
